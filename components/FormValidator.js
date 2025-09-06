4a9671698c84d32f0e929bdd7e722cd8fb202ae0
Cedwards <cedwards2527@gmail.com>
2025-09-06 13:55:41 -0400

    Add consturctor to FormValidation class

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,1 +1,10 @@
-
+class FormValidator {
+  constructor(settings, formEl) {
+    this._inputSelector = settings.inputSelector;
+    this._formSelector = settings.formSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formEl = formEl;
+  }
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import {
   todoTemplateSelector,
 } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
+import { FormValidator } from "../components/FormValidator.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -56,3 +57,6 @@ initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
+
+const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
+newTodoValidator.enableValidation();
