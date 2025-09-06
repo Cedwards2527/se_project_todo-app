058a5868e2577c50ed7f2546e8225bd784eda361
Cedwards <cedwards2527@gmail.com>
2025-09-06 09:52:40 -0400

    refactor todo date method

--- components/Todo.js
+++ components/Todo.js
@@ -20,19 +20,31 @@ class Todo {
     this._todoCheckboxEl.id = `todo-${this._data.id}`;
     this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
   }
+  _generateTodoDate() {
+    const todoDate = this._todoElement.querySelector(".todo__date");
+    const dueDate = new Date(this._data.date);
+    if (!isNaN(dueDate)) {
+      todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
+  }
 
   getView() {
     this._todoElement = this._templateElement.content
       .querySelector(".todo")
       .cloneNode(true);
     const todoNameEl = this._todoElement.querySelector(".todo__name");
-    const todoDate = this._todoElement.querySelector(".todo__date");
+
     this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
     todoNameEl.textContent = this._data.name;
 
     this._generateCheckboxEl();
     this._setEventListeners();
+    this._generateTodoDate();
 
     return this._todoElement;
   }
