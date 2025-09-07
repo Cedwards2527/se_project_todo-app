57eae55f9ab6f36e3bcc13cff3cbdf216368f800
Cedwards <cedwards2527@gmail.com>
2025-09-07 10:42:34 -0400

    Add  toggleButtonState and checkInputValidity to FormValidator

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -8,3 +8,50 @@ class FormValidator {
     this._inactiveButtonClass = settings.inactiveButtonClass;
     this._formEl = formEl;
   }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      showInputError(
+        this._formEl,
+        inputElement,
+        inputElement.validationMessage,
+        this._errorClass
+      );
+    } else {
+      hideInputError(this._formEl, inputElement);
+    }
+  }
+  _toggleButtonState = (inputElement) => {
+    if (hasInvalidInput(this._inputList)) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  };
+
+  _setEventListeners() {
+    this._inputList = this._formEl.querySelectorAll(this._inputSelector);
+    this._buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
+
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+}
+export { FormValidator };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "https://cdn.skypack.dev/uuid";
+//import { v4 as uuidv4 } from "https://cdn.skypack.dev/uuid";
 import {
   initialTodos,
   validationConfig,
